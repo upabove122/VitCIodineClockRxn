050ef0e380700f37b059880e6f998b8b561c780b
upabove122 <matsam158@gmail.com>
2019-10-09 19:05:33 +0800

    Update to plot.js

--- plot.js
+++ plot.js
@@ -1,12 +1,37 @@
-var i,
-    xy = [[0], [], [], []];
-    //xy[0] = 
+if (confirm('Please run this on a browser without any important tabs, as placing in crazy n values will crash the browser! Cancle to stop any processing')) {
+    CalculateXY(0.005, 1, 0, 1.5, 0.02, 20, 100000);
+    document.getElementById('n').value = 100000;
+    document.getElementById('dt').value = 0.005;      
+    Plotly.newPlot('myDiv', data, {});
+} else {
+    document.getElementById('n').style.display = 'none';
+    document.getElementById('dt').style.display = 'none';
+}
 
-CalculateXY(0.005, 1, 0, 1.5, 0.02, 20, 100000);
+var i,
+    xy = [[0], [], [], []],
+    trace1,
+    trace2,
+    trace3,
+    data;
+
+function buttonpress() {
+    'use strict';
+    var newdt = Number(document.getElementById('dt').value),
+        newn = Number(document.getElementById('n').value);
+    if (newn > 100000) {
+        if (confirm('Are you sure you want to proceed? n number is very large, it may take some time to calculate!')) {
+            CalculateXY(newdt, 1, 0, 1.5, 0.02, 20, newn);
+        }
+    } else {
+        CalculateXY(newdt, 1, 0, 1.5, 0.02, 20, newn);
+    }
+}
 
 function CalculateXY(dt, ao, bo, co, ko, ki, n) {
-    'use strict';
+    'use strict';    
     // where first array holds x axis and 2nd array holds the y axis
+    xy = [[0], [], [], []],
     xy[1][0] = ao,
     xy[2][0] = bo,
     xy[3][0] = co;
@@ -25,40 +50,42 @@ function CalculateXY(dt, ao, bo, co, ko, ki, n) {
         xy[2][i] = oldb + dt * ((ko * Math.pow(olda, 2)) - (ki * oldb * oldc));
         
         xy[3][i] = oldc + dt * ((-ki) * oldb * oldc);
-    }    
+    }   
+    console.log(xy);
+    datareset();
+    alert('All calculations done!');
 }
 
+function datareset(){
+    'use strict';
+    trace1 = {
+      x: xy[0],
+      y: xy[1],
+      mode: 'lines',
+      name: 'A',
+      text: ['A'],
+      line: {shape: 'spline'},
+      type: 'scatter'
+    };
+    trace2 = {
+      x: xy[0],
+      y: xy[2],
+      mode: 'lines',
+      name: 'B',
+      text: ['B'],
+      line: {shape: 'spline'},
+      type: 'scatter'
+    };
+    trace3 = {
+      x: xy[0],
+      y: xy[3],
+      mode: 'lines',
+      name: 'C',
+      text: ['C'],
+      line: {shape: 'spline'},
+      type: 'scatter'
+    };
 
-console.log(xy);
-
-var trace1 = {
-  x: xy[0],
-  y: xy[1],
-  mode: 'lines',
-  name: 'A',
-  text: ['A'],
-  line: {shape: 'spline'},
-  type: 'scatter'
-};
-var trace2 = {
-  x: xy[0],
-  y: xy[2],
-  mode: 'lines',
-  name: 'B',
-  text: ['B'],
-  line: {shape: 'spline'},
-  type: 'scatter'
-};
-var trace3 = {
-  x: xy[0],
-  y: xy[3],
-  mode: 'lines',
-  name: 'C',
-  text: ['C'],
-  line: {shape: 'spline'},
-  type: 'scatter'
-};
-
-var data = [trace1, trace2, trace3];
-
-Plotly.newPlot('myDiv', data, {});
+    data = [trace1, trace2, trace3];
+    Plotly.react('myDiv', data, {});
+}
